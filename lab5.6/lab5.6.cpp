225bc5da513964ea2823fa42771ef44d98ecf4a5
romikvers <73947323+romikvers@users.noreply.github.com>
2021-01-26 19:34:46 +0200

    lab5-6

---
+++ lab5.6/lab5.6.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+double h(const double a, const double b); // прототип
+
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+
+	double c = ((pow(h, 3)(t * t, 1) + h(1, t * (s * s))) / (1 + pow(h, 2)(s, t)));
+
+	cout << "c = " << c << endl;
+	return 0;
+}
+
+double h(const double a, const double b)
+{
+	return (pow(a, 2) * sin(b) + pow(b, 2) * cos(a));
+}
